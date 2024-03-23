608908eb75db4647a04e2f6a264c328b28ace9b1
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-23 13:26:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 //your code here!
+let listItem=document.getElementById("infi-list");
+let index=1;
+let loadingExtraItems = false; 
+
+
+function extraItems(){
+    for (i = 0; i < 2; i++) {
+        let list1 = document.createElement("li");
+        list1.innerText = "nik" + index++;
+        listItem.appendChild(list1);
+    } 
+	extraItems();
+};
+// extraItems();
+
+function addItems() {
+    for (i = 0; i < 10; i++) {
+        let list = document.createElement("li");
+        list.innerText = "nik" + index++;
+        listItem.appendChild(list);
+    }
+};
+
+addItems();
+
+
+
+
+listItem.addEventListener('scroll', function() {
+    if (listItem.scrollTop + listItem.clientHeight >= listItem.scrollHeight) {
+        extraItems(); // Load more items when scrolled to the bottom
+        // console.log("reached end");
+        // addItems();
+        
+    }
+    
+});
+
 
